1099d7a2667ea7e0ae3dc48613f03737d870a63e
JonnathanE <jonnathan.espinoza@unl.edu.ec>
2022-05-07 17:11:56 -0500

    Create FeaturedJobs section

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import FeaturedJobs from '../components/FeaturedJobs';
 import Hero from '../components/Hero';
 
 export default function Home() {
@@ -10,8 +11,10 @@ export default function Home() {
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
 			<Hero />
-			<main>
-				<p className='font-bold underline'>Hello world!</p>
+			<main className='mx-auto max-w-screen-xl px-6 sm:px-8'>
+				<div className='mt-16 space-y-20'>
+					<FeaturedJobs />
+				</div>
 			</main>
 		</>
 	);
