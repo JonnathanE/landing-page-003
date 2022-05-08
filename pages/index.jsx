ea38204f796f2e974e49356fd9d9fce9eaa56bb8
JonnathanE <jonnathan.espinoza@unl.edu.ec>
2022-05-07 20:05:57 -0500

    Create Footer Section

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import FeaturedJobs from '../components/FeaturedJobs';
+import Footer from '../components/Footer';
 import Hero from '../components/Hero';
 import TopCompanies from '../components/TopCompanies';
 
@@ -18,6 +19,7 @@ export default function Home() {
 					<TopCompanies />
 				</div>
 			</main>
+			<Footer />
 		</>
 	);
 }
